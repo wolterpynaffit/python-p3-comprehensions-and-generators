a6f5f922465851314cfdb5170293afaf9eb4da00
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-08-26 23:52:21 -0400

    finished

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # # returns an empty list if odd
+    is_even = [n for n in num_list if n % 2 == 0]
+    return (is_even)
+
 
 def make_exclamation(sentence_list):
-    pass
+    add_exclamation = [item +
+                       '!' for item in sentence_list if (len(item)) != 0]
+    return add_exclamation
